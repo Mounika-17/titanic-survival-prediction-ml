eee9363975f2c7eddf3537a9a036ff3cc26f05bf
Mounika-17 <maradana.mounika17@gmail.com>
2025-10-15 09:01:43 +0530

    added comments in data_ingestion.py file and updated notebook

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -38,7 +38,7 @@ class DataIngestion:
 
             logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-
+            # Give the path where to save the file
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
